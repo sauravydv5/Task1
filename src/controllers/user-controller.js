cee176b87d33fb2f22831f69f6101ae71c7f02a8
Saurav Kumar <sauravkumar91937@gmail.com>
2025-09-08 01:26:03 +0530

    fix

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const { validationResult } = require("express-validator");
-const User = require("../Models/User.js");
+const User = require("../models/User.js");
 
 // Token generate function
 const signToken = (user) =>
